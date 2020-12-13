e9cbc65a93f8e55c226607f417ffe6459f79480a
Watson,Kelvin <kelvincswatson@gmail.com>
2020-12-13 13:21:33 -0800

    clip image, bold username

--- app/src/main/java/com/watsonsoft/messaging/inbox/component/InboxItem.kt
+++ app/src/main/java/com/watsonsoft/messaging/inbox/component/InboxItem.kt
@@ -1,14 +1,19 @@
 package com.watsonsoft.messaging.inbox.component
 
+import android.graphics.fonts.FontStyle
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.watsonsoft.messaging.R
@@ -20,16 +25,21 @@ fun InboxItem() {
     ) {
         Image(
             imageVector = vectorResource(id = R.drawable.ic_launcher_background),
-            modifier = Modifier.size(dimensionResource(id = R.dimen.avatarSize))
+            modifier = Modifier.clip(shape = CircleShape)
+                .size(dimensionResource(id = R.dimen.avatarSize)),
+            contentScale = ContentScale.Crop,
         )
-        Column {
-
+        Column(
+            modifier = Modifier.padding(start = 16.dp)
+        ) {
+            Text("Sender username", maxLines = 1, style = TextStyle(fontWeight = FontWeight.Bold))
+            Text("Message preview...")
         }
     }
 }
 
 @Preview
 @Composable
-fun PreviewInboxItem(){
+fun PreviewInboxItem() {
     InboxItem()
 }
